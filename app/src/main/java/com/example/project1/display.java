fcf25ef031ab1e25ac668a4f7f3e07733191922c
H01i2 <851587379@qq.com>
2020-05-01 15:14:58 +0800

    dialog的布局

--- app/src/main/java/com/example/project1/display.java
+++ app/src/main/java/com/example/project1/display.java
@@ -1,12 +1,13 @@
 package com.example.project1;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.view.View;
-import android.widget.Toast;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.example.project1.Activity.GameActivity;
-import com.example.project1.Fragment.InfoFragment;
 import com.example.project1.Role.Role;
 import com.example.project1.db.getDB;
 
@@ -21,56 +22,92 @@ public class display extends View {
 
     public void dialog(final getDB DB, int EventsNum, final Role role) {
         if (EventsNum==1){
-            AlertDialog.Builder Events = new AlertDialog.Builder(this.getContext());
-            Events.setTitle("事件")
-                    .setMessage(DB.getEvent())
-                    .setNegativeButton(DB.getOptionT().get(0), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            DB.setNUM(role,1);
-                        }
-                    });
+            final AlertDialog Events = new AlertDialog.Builder(this.getContext()).create();
             Events.show();
+            Window window = Events.getWindow();
+            window.setContentView(R.layout.dialog_1);
+            TextView dialogTitle = (TextView) window.findViewById(R.id.tv_dialog1_title);
+            dialogTitle.setText("事件");
+            TextView dialogContent = (TextView) window.findViewById(R.id.tv_dialog1_content);
+            dialogContent.setText(DB.getEvent());
+            Button btn_1 = (Button) window.findViewById(R.id.btn_dialog1_1);
+            btn_1.setText(DB.getOptionT().get(0));
+                btn_1.setOnClickListener(new View.OnClickListener(){
+
+                    @Override
+                    public void onClick(View v) {
+                        DB.setNUM(role,1);
+                        Events.dismiss();
+                    }
+                });
         }else if(EventsNum==2){
-            AlertDialog.Builder Events = new AlertDialog.Builder(this.getContext());
-            Events.setTitle("事件");
-            Events.setMessage(DB.getEvent());
-            Events.setPositiveButton(DB.getOptionT().get(0), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    DB.setNUM(role,1);
-                }
-            });
-            Events.setNegativeButton(DB.getOptionT().get(1), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    DB.setNUM(role,2);
-                }
-            });
+            final AlertDialog Events = new AlertDialog.Builder(this.getContext()).create();
             Events.show();
+            Window window = Events.getWindow();
+            window.setContentView(R.layout.dialog_2);
+            TextView dialogTitle = (TextView) window.findViewById(R.id.tv_dialog2_title);
+            dialogTitle.setText("事件");
+            TextView dialogContent = (TextView) window.findViewById(R.id.tv_dialog2_content);
+            dialogContent.setText(DB.getEvent());
+            Button btn_1 = (Button) window.findViewById(R.id.btn_dialog2_1);
+            btn_1.setText(DB.getOptionT().get(0));
+            btn_1.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    DB.setNUM(role,1);
+                    Events.dismiss();
+                }
+            });
+            Button btn_2 = (Button) window.findViewById(R.id.btn_dialog2_2);
+            btn_2.setText(DB.getOptionT().get(1));
+            btn_2.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    DB.setNUM(role,2);
+                    Events.dismiss();
+                }
+            });
         }else if (EventsNum==3){
-            AlertDialog.Builder Events = new AlertDialog.Builder(this.getContext());
-            Events.setTitle("事件");
-            Events.setMessage(DB.getEvent());
-            Events.setPositiveButton(DB.getOptionT().get(0), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    DB.setNUM(role,1);
-                }
-            });
-            Events.setNegativeButton(DB.getOptionT().get(1), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    DB.setNUM(role,2);
-                }
-            });
-            Events.setNeutralButton(DB.getOptionT().get(2), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    DB.setNUM(role,3);
-                }
-            });
+            final AlertDialog Events = new AlertDialog.Builder(this.getContext()).create();
             Events.show();
+            Window window = Events.getWindow();
+            window.setContentView(R.layout.dialog_3);
+            TextView dialogTitle = (TextView) window.findViewById(R.id.tv_dialog3_title);
+            dialogTitle.setText("事件");
+            TextView dialogContent = (TextView) window.findViewById(R.id.tv_dialog3_content);
+            dialogContent.setText(DB.getEvent());
+            Button btn_1 = (Button) window.findViewById(R.id.btn_dialog3_1);
+            btn_1.setText(DB.getOptionT().get(0));
+            btn_1.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    DB.setNUM(role,1);
+                    Events.dismiss();
+                }
+            });
+            Button btn_2 = (Button) window.findViewById(R.id.btn_dialog3_2);
+            btn_2.setText(DB.getOptionT().get(1));
+            btn_2.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    DB.setNUM(role,2);
+                    Events.dismiss();
+                }
+            });
+            Button btn_3 = (Button) window.findViewById(R.id.btn_dialog3_3);
+            btn_3.setText(DB.getOptionT().get(2));
+            btn_3.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    DB.setNUM(role,3);
+                    Events.dismiss();
+                }
+            });
         }
 
     }
